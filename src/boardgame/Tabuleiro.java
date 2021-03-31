5e613b02f74b82f5866b90a25fc9b1d391d922d1
Henry T. Garcia <henrytgarcia@uol.com.br>
2021-03-31 14:36:51 -0300

    Starting to implement Tabuleiro e Piece

---
+++ src/boardgame/Tabuleiro.java
@@ -0,0 +1,35 @@
+package boardgame;
+
+public class Tabuleiro {
+	//Atributos
+	private int rows;
+	private int colunms;
+	private Piece[][] pieces;
+	
+	//Construtores
+	public Tabuleiro(int rows, int colunms) {
+		this.rows = rows;
+		this.colunms = colunms;
+		pieces = new Piece[rows][colunms];
+	}
+
+	//Gets e Sets
+	public int getRows() {
+		return rows;
+	}
+
+	public void setRows(int rows) {
+		this.rows = rows;
+	}
+
+	public int getColunms() {
+		return colunms;
+	}
+
+	public void setColunms(int colunms) {
+		this.colunms = colunms;
+	}
+	
+	
+	
+}
