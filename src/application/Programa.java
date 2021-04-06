c2e6f8b910bf976c2f97524b5e1df36e6b3f5473
Henry T. Garcia <henrytgarcia@uol.com.br>
2021-04-06 15:16:54 -0300

    Checkmate logic

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -20,7 +20,7 @@ public class Programa {
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
 		
-		while(true) {
+		while(!chessMatch.getChechMate()) {
 			try {
 				UI.clearScreen();
 				UI.printMatch(chessMatch, captured);
@@ -53,6 +53,8 @@ public class Programa {
 			
 			
 		}
+		UI.clearScreen();
+		UI.printMatch(chessMatch, captured);
 	}
 
 }
